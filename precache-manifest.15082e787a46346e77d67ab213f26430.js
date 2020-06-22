64a882dbe485c0df5a66d7f1a7469b7d7a989fa7
Molosh Nikita <nmiokliotsah@gmail.com>
2020-06-22 11:19:52 +0300

    Updates

---
+++ precache-manifest.15082e787a46346e77d67ab213f26430.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5058b7ed1b17eb91f1800a90931c97b0",
+    "url": "/-astronomy_picture-/index.html"
+  },
+  {
+    "revision": "706a21cd7505b1ad109e",
+    "url": "/-astronomy_picture-/static/css/main.a30fdc73.chunk.css"
+  },
+  {
+    "revision": "737422e978d64434b2f6",
+    "url": "/-astronomy_picture-/static/js/2.2a6ce7a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/-astronomy_picture-/static/js/2.2a6ce7a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "706a21cd7505b1ad109e",
+    "url": "/-astronomy_picture-/static/js/main.49305ed6.chunk.js"
+  },
+  {
+    "revision": "6c1aa12db8b04fdfbb8a",
+    "url": "/-astronomy_picture-/static/js/runtime-main.087c5073.js"
+  }
+]);
